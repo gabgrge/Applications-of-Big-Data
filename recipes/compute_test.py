2754a4eb73bc0d973f855274eaa9ee80582f4b72
admin <gabriel.george@efrei.net>
2023-11-29 14:02:06 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_test

---
+++ recipes/compute_test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read "cleaned_data" DataFrame
+cleaned_data = dataiku.Dataset("cleaned_data")
+listenings_df = cleaned_data.get_dataframe()
+
+# Compute "datetime" DataFrame
+datetime_df = listenings_df[["datetime"]].drop_duplicates().sort_values(ignore_index=True, by="datetime", ascending=False)
+datetime_df["date"] = datetime_df["datetime"].dt.date
+datetime_df["time"] = datetime_df["datetime"].dt.time
+datetime_df["year"] = datetime_df["datetime"].dt.year
+datetime_df["month"] = datetime_df["datetime"].dt.month
+datetime_df["week"] = datetime_df["datetime"].dt.week
+datetime_df["day"] = datetime_df["datetime"].dt.day
+
+
+# Write recipe outputs
+test = dataiku.Dataset("test")
+test.write_with_schema(datetime_df)
