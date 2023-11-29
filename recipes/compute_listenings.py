5140452657f5f48fa28dfdb733458d34addfe1f3
admin <gabriel.george@efrei.net>
2023-11-29 12:29:37 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_listenings

--- recipes/compute_listenings.py
+++ recipes/compute_listenings.py
@@ -8,11 +8,7 @@ cleaned_data = dataiku.Dataset("cleaned_data")
 cleaned_data_df = cleaned_data.get_dataframe()
 
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-listenings_df = cleaned_data_df # For this sample code, simply copy input to output
+listenings_df = cleaned_data_df
 
 
 # Write recipe outputs
