0c9b0f75da6aab3c7d9c460dc78c7414f5ab7679
admin <gabriel.george@efrei.net>
2023-11-30 21:39:20 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_most_listened_artist_week

--- recipes/compute_most_listened_artist_week.sql
+++ recipes/compute_most_listened_artist_week.sql
@@ -1,2 +1,22 @@
-SELECT *
-  FROM "listenings"
+-- Most Listened Artist for Each Week
+WITH ranked_albums AS (
+    SELECT
+        EXTRACT(YEAR FROM datetime) AS year,
+        EXTRACT(WEEK FROM datetime) AS week,
+        album,
+        artist,
+        COUNT(*) AS listenings,
+        ROW_NUMBER() OVER (PARTITION BY EXTRACT(YEAR FROM datetime), EXTRACT(WEEK FROM datetime) ORDER BY COUNT(*) DESC) AS album_rank
+    FROM listenings
+    WHERE album IS NOT NULL
+    GROUP BY year, week, album, artist
+)
+SELECT
+    year,
+    week,
+    album,
+    artist,
+    listenings
+FROM ranked_albums
+WHERE album_rank = 1
+ORDER BY year, week, listenings DESC;
