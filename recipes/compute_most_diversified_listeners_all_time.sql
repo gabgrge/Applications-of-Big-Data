dd5a5e0e8326906c037677fc29489b7b919304c7
admin <gabriel.george@efrei.net>
2023-11-30 21:48:36 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_most_diversified_listeners_all_time

--- recipes/compute_most_diversified_listeners_all_time.sql
+++ recipes/compute_most_diversified_listeners_all_time.sql
@@ -1,2 +1,8 @@
-SELECT *
-  FROM "listenings"
+-- Ranking
+SELECT
+    usr,
+    COUNT(DISTINCT artist) AS unique_artists_count
+FROM listenings
+GROUP BY usr
+ORDER BY unique_artists_count DESC
+LIMIT 1;
