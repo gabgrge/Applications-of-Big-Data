a86ed4b58992d2973b259777219398852d76a2d3
admin <gabriel.george@efrei.net>
2023-11-30 21:28:58 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_biggest_listeners_week

--- recipes/compute_biggest_listeners_week.sql
+++ recipes/compute_biggest_listeners_week.sql
@@ -1,2 +1,9 @@
-SELECT *
-  FROM "listenings"
+-- Ranking of 10 Biggest Listeners for Each Week
+SELECT
+    'top_10_listeners_weekly' AS kpi_name,
+    COUNT(*) AS result_value
+FROM listenings
+WHERE EXTRACT(WEEK FROM datetime) = EXTRACT(WEEK FROM CURRENT_DATE)
+GROUP BY usr
+ORDER BY result_value DESC
+LIMIT 10;
