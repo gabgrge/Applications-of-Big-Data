4f58cc5bed3ac0f8196aeb7990036f8336794c52
admin <gabriel.george@efrei.net>
2023-11-30 21:09:10 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_most_listened_track_week

--- recipes/compute_most_listened_track_week.sql
+++ recipes/compute_most_listened_track_week.sql
@@ -1,23 +1,22 @@
 -- Most Listened Track for Each Week
 
-WITH ranked_listenings AS (
+WITH ranked_tracks AS (
     SELECT
         EXTRACT(YEAR FROM datetime) AS year,
         EXTRACT(WEEK FROM datetime) AS week,
         track,
         artist,
         COUNT(*) AS listenings,
-        ROW_NUMBER() OVER (PARTITION BY year, week ORDER BY COUNT(*) DESC) AS rank
+        ROW_NUMBER() OVER (PARTITION BY EXTRACT(YEAR FROM datetime), EXTRACT(WEEK FROM datetime) ORDER BY COUNT(*) DESC) AS track_rank
     FROM listenings
     GROUP BY year, week, track, artist
 )
-
 SELECT
     year,
     week,
     track,
     artist,
     listenings
-FROM ranked_listenings
-WHERE rank <= 10
-ORDER BY year, week, rank;
+FROM ranked_tracks
+WHERE track_rank <= 10
+ORDER BY year, week, listenings DESC;
