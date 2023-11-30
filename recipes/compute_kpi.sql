f6e0c0af8f74ad0e0ee093c136529bb65ee1f636
admin <gabriel.george@efrei.net>
2023-11-30 17:10:52 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_kpi

--- recipes/compute_kpi.sql
+++ recipes/compute_kpi.sql
@@ -25,14 +25,6 @@ GROUP BY track
 ORDER BY result_value DESC
 LIMIT 1;
 
--- Cross Tabulation of Listened Tracks by Listener and by Artist
-INSERT INTO kpi_results (kpi_name, result_value)
-SELECT
-    'cross_tabulation_tracks_by_listener_and_artist' AS kpi_name,
-    COUNT(*) AS result_value
-FROM listenings
-GROUP BY usr, artist;
-
 -- Ranking of 10 Biggest Listeners All Time
 INSERT INTO kpi_results (kpi_name, result_value)
 SELECT
