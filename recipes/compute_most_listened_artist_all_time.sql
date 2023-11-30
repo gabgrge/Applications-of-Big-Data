248c5501942e1d86399c061a16d1972b26e0904f
admin <gabriel.george@efrei.net>
2023-11-30 21:38:22 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_most_listened_artist_all_time

--- recipes/compute_most_listened_artist_all_time.sql
+++ recipes/compute_most_listened_artist_all_time.sql
@@ -1,2 +1,9 @@
-SELECT *
-  FROM "listenings"
+-- Most Listened Artist All Time
+SELECT
+    artist,
+    COUNT(*) AS listenings
+FROM listenings
+WHERE artist IS NOT NULL
+GROUP BY artist
+ORDER BY listenings DESC
+LIMIT 1;
