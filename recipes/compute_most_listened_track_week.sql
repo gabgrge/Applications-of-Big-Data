d3bc41f26036a87239a5ec291890d059b9104435
admin <gabriel.george@efrei.net>
2023-11-30 18:15:25 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_most_listened_track_week

--- recipes/compute_most_listened_track_week.sql
+++ recipes/compute_most_listened_track_week.sql
@@ -1,12 +1,11 @@
 -- Most Listened Track and Album for Each Week
 SELECT
-    EXTRACT(YEAR FROM datetime) as year,
-    EXTRACT(WEEK FROM datetime) as week,
+    EXTRACT(YEAR FROM datetime) AS year,
+    EXTRACT(WEEK FROM datetime) AS week,
     track,
     artist,
     COUNT(*) AS listenings
 FROM listenings
-WHERE EXTRACT(WEEK FROM datetime) = EXTRACT(WEEK FROM CURRENT_DATE)
-GROUP BY track, artist
+GROUP BY year, week, track, artist
 ORDER BY listenings DESC
 LIMIT 1;
