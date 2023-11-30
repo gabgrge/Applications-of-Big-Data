190e3c9bf2af41d14d655a5275e0acf193b9cd40
admin <gabriel.george@efrei.net>
2023-11-30 21:24:43 +0100

    Saved recipe: APPLICATIONSOFBIGDATA.compute_biggest_listeners_all_time

--- recipes/compute_biggest_listeners_all_time.sql
+++ recipes/compute_biggest_listeners_all_time.sql
@@ -1,2 +1,9 @@
-SELECT *
-  FROM "listenings"
+-- Ranking of 10 Biggest Listeners All Time
+INSERT INTO kpi_results (kpi_name, result_value)
+SELECT
+    'top_10_listeners_all_time' AS kpi_name,
+    COUNT(*) AS result_value
+FROM listenings
+GROUP BY usr
+ORDER BY result_value DESC
+LIMIT 10;
