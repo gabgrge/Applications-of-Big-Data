1fabcef48c1578c3bdc6a63b26bbd0a327c35ee0
admin <gabriel.george@efrei.net>
2023-12-07 15:57:00 +0100

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -4,7 +4,7 @@ import pandas as pd, numpy as np
 def is_artist_unknown(a):
     if pd.isna(a):
         return np.nan
-    if a == "<Unknown>":
+    if a in ["<Unknown>", "[unknown]"]:
         return True
     else:
         return False
